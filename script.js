eb1878c8bacd858857c19dcb3f014e28e5349d6b
abigailhdez <95898495+abigailhdez@users.noreply.github.com>
2022-01-16 21:43:50 -0600

    feliprueba

---
+++ script.js
@@ -0,0 +1,16 @@
+let carreritas = ["lucia", "roberto", "maria", "jesus", "andrea", "jose"]
+carreritas
+
+console.log(carreritas)
+
+let carreritas2
+
+carreritas.splice(2,4, "andrea", "maria", "jesus", "jose")
+carreritas.pop()
+//'lucia', 'roberto', 'andrea', 'maria', 'jesus']
+
+carreritas.splice(1,4,"cristobal","fernando", "armando","andrea", "maria", "jesus", "jose")
+
+carreritas.unshift("federico")
+console.log(carreritas)
+// 2
